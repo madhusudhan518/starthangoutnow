2500b772fb218092257ed5387fc0124e9c9552da
Madhu <madhusudhan@coppernine.com>
2015-04-16 19:48:50 +0530

    added xml file working now

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'users/show'
   get 'users/hangout-sample' => 'users#hangout-sample'
+  get 'users/hangout-on-air' => 'users#hangout-on-air'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
